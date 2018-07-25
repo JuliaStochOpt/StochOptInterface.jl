e2fc2095f9773e9295e83446fee13000ba65cf5c
Benoît Legat <benoit.legat@gmail.com>
2018-07-25 16:18:13 +0200

    Use result.status

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -20,7 +20,7 @@ function optimize!(sp::AbstractStochasticProgram, algo::AbstractAlgorithm,
         if verbose >= 3
             print_iteration_summary(info)
         end
-        if getstatus(result.paths.paths[1].sol_scenario[1]) != :Infeasible
+        if result.status != :Infeasible
             break
         end
     end
